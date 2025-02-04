5b3ce1daf084139b8678c19b26175b5d35ba2aba
LucaAgostinho <lucaabenda@icloud.com>
2025-02-03 23:05:45 -0300

    feat: add model viewset

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import CarViewSet
+
+router = DefaultRouter()
+router.register(r'car', CarViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer, CarSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Car
 
 # Create your views here.
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class CarViewSet(ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
