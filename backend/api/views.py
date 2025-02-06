d6a513fac5bfbc42b118ed9cc2978d6d91e86f98
LucaAgostinho <lucaabenda@icloud.com>
2025-02-05 23:20:52 -0300

    feat: set permissions on Car view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 from .serializers import UserSerializer, CarSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from .models import Car
 
 # Create your views here.
@@ -15,3 +14,10 @@ class CreateUserView(generics.CreateAPIView):
 class CarViewSet(ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy', 'update', 'partial_update']:
+            self.permission_classes = [IsAuthenticated, IsAdminUser]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
